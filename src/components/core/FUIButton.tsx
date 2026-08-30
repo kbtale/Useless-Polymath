9a10729cc5018b9bc8a7013074045521ec5b6910
kbtale <cebemind@hotmail.com>
2026-08-30 18:12:13 -0400

    refactor(types): strict type FUIButton and CoreDateInput with prop forwarding

--- src/components/core/FUIButton.tsx
+++ src/components/core/FUIButton.tsx
@@ -1,19 +1,21 @@
-import React from 'react';
-import styles from './FUIButton.module.scss';
 import clsx from 'clsx';
+import type React from 'react';
+import styles from './FUIButton.module.scss';
 
-interface FUIButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+export interface FUIButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   variant?: 'solid' | 'outline';
 }
 
 export const FUIButton: React.FC<FUIButtonProps> = ({
   children,
   variant = 'outline',
+  type = 'button',
   className,
   ...props
 }) => {
   return (
     <button
+      type={type}
       className={clsx(styles.fuiButton, variant === 'solid' && styles.solid, className)}
       {...props}
     >
